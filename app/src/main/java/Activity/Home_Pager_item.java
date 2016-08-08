67eee3a7f8328426f3f64ad39a27f22bf160f89a
xinda_05 <1990728448@qq.com>
2016-08-09 00:46:34 +0800

    1.0

--- app/src/main/java/Activity/Home_Pager_item.java
+++ app/src/main/java/Activity/Home_Pager_item.java
@@ -1,7 +1,12 @@
-package Activity;
+package activity;
+
+import android.app.Activity;
 
 /**
  * Created by Jerry Mouse on 2016/8/9.
  */
-public class Home_Pager_item {
+public class Home_Pager_item extends Activity{
+
+
+
 }
