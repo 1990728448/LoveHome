9187f61082799cff101559c848c170e274775491
xinda_05 <1990728448@qq.com>
2016-08-09 11:07:16 +0800

    1.1

---
+++ my/src/main/java/org/example/xinda_05/my/ss.java
@@ -0,0 +1,7 @@
+package org.example.xinda_05.my;
+
+/**
+ * Created by Jerry Mouse on 2016/8/9.
+ */
+public class ss {
+}
---
+++ release/src/main/java/org/example/xinda_05/release/aa.java
@@ -0,0 +1,7 @@
+package org.example.xinda_05.release;
+
+/**
+ * Created by Jerry Mouse on 2016/8/9.
+ */
+public class aa {
+}
