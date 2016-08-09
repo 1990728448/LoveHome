1e8a0391b16dbf0dda151d5f0f63a09e4f8047c9
Your Name <you@example.com>
2016-08-09 11:39:03 +0800

    xml1

--- release/src/main/java/org/example/xinda_05/release/aa.java
+++
@@ -1,7 +0,0 @@
-package org.example.xinda_05.release;
-
-/**
- * Created by Jerry Mouse on 2016/8/9.
- */
-public class aa {
-}
---
+++ release/src/main/java/org/example/xinda_05/release/activity/activity/MainActivity_Release.java
@@ -0,0 +1,7 @@
+package org.example.xinda_05.release.activity.activity;
+
+/**
+ * Created by Administrator on 2016/8/9 0009.
+ */
+public class MainActivity_Release {
+}
