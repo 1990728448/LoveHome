f99a77e188a53e35c8a3d577dc356208c29406dc
xinda_05 <1990728448@qq.com>
2016-08-09 12:54:18 +0800

    1.1

--- homepager/src/main/java/org/example/xinda_05/homepager/fragment/homepager/activity/Home_Pager_to_item.java
+++ homepager/src/main/java/org/example/xinda_05/homepager/fragment/homepager/activity/Home_Pager_to_item.java
@@ -9,7 +9,7 @@ import org.example.xinda_05.homepager.R;
 /**
  * Created by Jerry Mouse on 2016/8/9.
  */
-public class Home_Pager_item extends Activity{
+public class Home_Pager_to_item extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
