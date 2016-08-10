882b9e528f1006f60e5042fb1a1dc5728df8ed4f
Lyx1996 <497862115@qq.com>
2016-08-10 10:17:17 +0800

    2.0

--- my/src/main/java/org/example/xinda_05/my/My_releaseActivity.java
+++ my/src/main/java/org/example/xinda_05/my/My_releaseActivity.java
@@ -2,6 +2,9 @@ package org.example.xinda_05.my;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 /**
  * Created by Jerry Mouse on 2016/8/9.
@@ -12,12 +15,17 @@ public class My_releaseActivity extends Activity{
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.my_about_layout);
-            findViewById(R.id.fanhui);
+            Button bt1=(Button) findViewById(R.id.back);
+            bt1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
 
+                }
+               });//点击后跳回到上一页面
         }
-
-
-
 }
+
+
+
 
 
