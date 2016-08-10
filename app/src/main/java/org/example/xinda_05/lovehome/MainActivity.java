093fa89962ede16553be9c24386e2c87cba137cc
xinda_05 <1990728448@qq.com>
2016-08-10 17:17:12 +0800

    1.2.2

--- app/src/main/java/org/example/xinda_05/lovehome/MainActivity.java
+++ app/src/main/java/org/example/xinda_05/lovehome/MainActivity.java
@@ -11,43 +11,45 @@ import android.widget.LinearLayout;
 import org.example.xinda_05.homepager.fragment.homepager.fragment.Home_pager_content_Fragment;
 import org.example.xinda_05.homepager.fragment.homepager.fragment.Home_pager_title_Fragment;
 import org.example.xinda_05.release.activity.fragment.Release_page_tatle_Fragment;
-import org.example.xinda_05.release.activity.fragment.Release_pager_content_Fragment;
 
 public class MainActivity extends FragmentActivity {
 
     private FragmentManager fm; //碎片管理器
     private FragmentTransaction ft;//碎片事务
-    private ImageView HomePager_button_lovehome_image,HomePager_button_shared_image,HomePager_button_my_image;
-    private LinearLayout HomePager_button_loveHome,HomePager_button_shared,HomePager_button_my;
+    private ImageView HomePager_button_lovehome_image, HomePager_button_shared_image, HomePager_button_my_image;
+    private LinearLayout HomePager_button_loveHome, HomePager_button_shared, HomePager_button_my;
 
 
-    /*
- * 用于底部选项颜色的更改
- * @author Jerry Mouse
- * @time 2016/8/10 11:09
- * @effect
- */
-    View.OnClickListener click=new View.OnClickListener() {
+    /**
+     * 用于底部选项颜色的更改
+     * @author Jerry Mouse
+     * @time 2016/8/10 11:09
+     * @effect
+     */
+    View.OnClickListener click = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             HomePager_button_lovehome_image.setImageResource(R.mipmap.home);
             HomePager_button_shared_image.setImageResource(R.mipmap.publish);
             HomePager_button_my_image.setImageResource(R.mipmap.wode);
-            FragmentTransaction ft2=fm.beginTransaction();
-            switch (view.getId()){
-                case R.id.HomePager_button_loveHome:{
+            FragmentTransaction ft2 = fm.beginTransaction();
+            switch (view.getId()) {
+                case R.id.HomePager_button_loveHome: {
                     HomePager_button_lovehome_image.setImageResource(R.mipmap.home_press);
-                    ft2.replace(R.id.HomePager_layout_title,new Home_pager_title_Fragment());
-                    ft2.replace(R.id.HomePager_layout_content,new Home_pager_content_Fragment());
-                }break;
-                case R.id.HomePager_button_shared:{
+                    ft2.replace(R.id.HomePager_layout_title, new Home_pager_title_Fragment());
+                    ft2.replace(R.id.HomePager_layout_content, new Home_pager_content_Fragment());
+                }
+                break;
+                case R.id.HomePager_button_shared: {
                     HomePager_button_shared_image.setImageResource(R.mipmap.publish_press);
-                    ft2.replace(R.id.HomePager_layout_title,new Release_page_tatle_Fragment());
-                    ft2.replace(R.id.HomePager_layout_content,new Release_pager_content_Fragment());
-                }break;
-                case R.id.HomePager_button_my:{
+                    ft2.replace(R.id.HomePager_layout_title, new Release_page_tatle_Fragment());
+                    //ft2.replace(R.id.HomePager_layout_content,new Release_pager_content_Fragment());
+                }
+                break;
+                case R.id.HomePager_button_my: {
                     HomePager_button_my_image.setImageResource(R.mipmap.wode_press);
-                }break;
+                }
+                break;
             }
             ft2.commit();
         }
@@ -56,7 +58,6 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
         setContentView(R.layout.activity_main);
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -67,31 +68,28 @@ public class MainActivity extends FragmentActivity {
         initView();
 
 
-
-
-
-
     }
-    /*
+
+    /**
      * 用来初始化控件
      * @author Jerry Mouse
      * @time 2016/8/9 2:28
      * @effect
      */
     private void initView() {
-        fm=getSupportFragmentManager();
-        ft=fm.beginTransaction();
-        ft.replace(R.id.HomePager_layout_title,new Home_pager_title_Fragment());
-        ft.replace(R.id.HomePager_layout_content,new Home_pager_content_Fragment());
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        ft.replace(R.id.HomePager_layout_title, new Home_pager_title_Fragment());
+        ft.replace(R.id.HomePager_layout_content, new Home_pager_content_Fragment());
         ft.commit();
 
         HomePager_button_lovehome_image = (ImageView) findViewById(R.id.HomePager_button_lovehome_image);
         HomePager_button_shared_image = (ImageView) findViewById(R.id.HomePager_button_shared_image);
         HomePager_button_my_image = (ImageView) findViewById(R.id.HomePager_button_my_image);
 
-        HomePager_button_loveHome= (LinearLayout) findViewById(R.id.HomePager_button_loveHome);
-        HomePager_button_shared= (LinearLayout) findViewById(R.id.HomePager_button_shared);
-        HomePager_button_my= (LinearLayout) findViewById(R.id.HomePager_button_my);
+        HomePager_button_loveHome = (LinearLayout) findViewById(R.id.HomePager_button_loveHome);
+        HomePager_button_shared = (LinearLayout) findViewById(R.id.HomePager_button_shared);
+        HomePager_button_my = (LinearLayout) findViewById(R.id.HomePager_button_my);
         HomePager_button_loveHome.setOnClickListener(click);
         HomePager_button_shared.setOnClickListener(click);
         HomePager_button_my.setOnClickListener(click);
