469f90910a63fc696cc110d6fe8c7469ab1743c1
Lyx1996 <497862115@qq.com>
2016-08-11 11:50:06 +0800

    2.1

--- my/src/main/java/org/example/xinda_05/my/My_login_headFragment.java
+++ my/src/main/java/org/example/xinda_05/my/My_login_headFragment.java
@@ -1,8 +1,9 @@
 package org.example.xinda_05.my;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,7 +11,7 @@ import android.view.ViewGroup;
 /**
  * Created by Administrator on 2016/8/10.
  */
-public class My_login_headFragment extends Fragment{
+public class My_login_headFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
