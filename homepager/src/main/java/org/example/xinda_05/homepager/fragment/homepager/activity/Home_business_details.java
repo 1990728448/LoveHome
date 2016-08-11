3c27f3b6b803bb0f0a759cd175beddc351537efc
Your Name <you@example.com>
2016-08-11 09:29:51 +0800

    1.1.6

--- homepager/src/main/java/org/example/xinda_05/homepager/fragment/homepager/activity/Home_business_details.java
+++ homepager/src/main/java/org/example/xinda_05/homepager/fragment/homepager/activity/Home_business_details.java
@@ -9,6 +9,8 @@ import org.example.xinda_05.homepager.R;
  * Created by ZhouZhicheng on 2016/8/10.
  */
 public class Home_business_details extends Activity{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
