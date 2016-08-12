bae48c2b2de5bd715947a8df2ba080dff4c1029a
Your Name <you@example.com>
2016-08-12 19:05:26 +0800

    1.1.6

--- homepager/src/main/java/org/example/xinda_05/homepager/fragment/homepager/activity/Home_business_details.java
+++ homepager/src/main/java/org/example/xinda_05/homepager/fragment/homepager/activity/Home_business_details.java
@@ -2,6 +2,8 @@ package org.example.xinda_05.homepager.fragment.homepager.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import org.example.xinda_05.homepager.R;
 
@@ -15,6 +17,17 @@ public class Home_business_details extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_business_details_layouth);
+        ImageButton fanhui= (ImageButton) findViewById(R.id.home_business_layouth_fanhui);
+        ImageButton shoucang= (ImageButton) findViewById(R.id.home_business_layouth_shoucang);
+        //结束当前界面
+        fanhui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
 
     }
 }
